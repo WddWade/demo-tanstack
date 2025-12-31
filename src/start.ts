897d8b4f77ff3a72066cb74f6ad50e632ff9be96
Wade <wade@wddgroup.com>
2025-12-31 09:05:49 +0800

    wade update

--- src/start.ts
+++ src/start.ts
@@ -7,9 +7,23 @@ const authMiddleware = createMiddleware({ type: "request" })
         return next()
         //...
     })
+const fnMiddleware = createMiddleware({ type: "function" })
+    .client(async ({ next }) => {
+        const result = await next()
+        // Woah! We have the time from the server!
+        console.log('Time from the server:', result.context.timeFromServer)
+
+        return result
+    })
+    .server(async ({ context, next }) => {
+        console.log("authMiddleware", request.url);
+        return next()
+        //...
+    })
 
 export const startInstance = createStart(() => {
     return {
+        functionMiddleware: [fnMiddleware],
         requestMiddleware: [authMiddleware],
     }
 }) 
