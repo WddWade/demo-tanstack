ebc136f24f26cbc9c81d47993f4791dbb491a35f
xwadex <5094484+WddWade@users.noreply.github.com>
2025-12-31 15:55:54 +0800

    Wade Update

--- src/start.ts
+++ src/start.ts
@@ -1,29 +1,32 @@
 // src/start.ts
 import { createMiddleware, createStart } from '@tanstack/react-start'
 
-const authMiddleware = createMiddleware({ type: "request" })
+const requestMiddleware = createMiddleware({ type: "request" })
     .server(async ({ request, next }) => {
-        console.log("authMiddleware", request.url);
+        console.log("authMiddleware.server", request.url);
         return next()
         //...
     })
-const fnMiddleware = createMiddleware({ type: "function" })
+
+const functionMiddleware = createMiddleware({ type: "function" })
     .client(async ({ next }) => {
         const result = await next()
         // Woah! We have the time from the server!
-        console.log('Time from the server:', result.context.timeFromServer)
+        console.log("functionMiddleware.client");
+        // console.log('Time from the server:', result.context.timeFromServer)
 
         return result
     })
     .server(async ({ context, next }) => {
-        console.log("authMiddleware", request.url);
+        // console.log("authMiddleware", request.url);
+        console.log("functionMiddleware.server");
         return next()
         //...
     })
 
 export const startInstance = createStart(() => {
     return {
-        functionMiddleware: [fnMiddleware],
-        requestMiddleware: [authMiddleware],
+        functionMiddleware: [functionMiddleware],
+        requestMiddleware: [requestMiddleware],
     }
 }) 
