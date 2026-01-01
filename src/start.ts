a64cd12cbd1f60dfa1ba65f6ccaa246aadef2c4f
Wade <wade@wddgroup.com>
2026-01-01 23:45:56 +0800

    wade update

--- src/start.ts
+++ src/start.ts
@@ -2,13 +2,17 @@
 import { createMiddleware, createStart } from '@tanstack/react-start'
 
 const requestMiddleware = createMiddleware({ type: "request" })
-    .server(async ({ request, next }) => {
-        console.log("authMiddleware.server", request.url);
-        return next()
+    .server(async ({ request, next, context }) => {
+        console.log("authMiddleware.server", context);
+
+        const result = await next()
+        result.response.headers.set('x-powered-by', 'tanstack')
+
+        return result
         //...
     })
 
-const functionMiddleware = createMiddleware({ type: "function" })
+const functionClientMiddleware = createMiddleware({ type: "function" })
     .client(async ({
         data,
         context,
@@ -20,18 +24,24 @@ const functionMiddleware = createMiddleware({ type: "function" })
         functionId,
     }) => {
         const nextContext = { name: "wade" }
-        return next({ sendContext: nextContext })
+        console.log("functionMiddleware.client");
+        const result = await next({ sendContext: nextContext })
+
+        return result
     })
+
+const functionServerMiddleware = createMiddleware({ type: "function" })
     .server(async ({ context, next }) => {
         // console.log("authMiddleware", request.url);
         console.log("functionMiddleware.server", context);
+
         return next()
         //...
     })
 
 export const startInstance = createStart(() => {
     return {
-        functionMiddleware: [functionMiddleware],
+        functionMiddleware: [functionClientMiddleware, functionServerMiddleware],
         requestMiddleware: [requestMiddleware],
     }
 }) 
